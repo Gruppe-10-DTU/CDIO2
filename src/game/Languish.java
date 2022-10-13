3e94dcac69d9e0cf8348dc128990b2881f1ce4c5
sand4570 <sand4570@stud.kea.dk>
2022-10-13 11:13:38 +0200

    importing txt file

--- src/game/Languish.java
+++ src/game/Languish.java
@@ -1,2 +1,29 @@
-package game;public class Languish {
+package game;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+
+public class Languish {
+
+    Path path = Paths.get("english.txt");
+    HashMap<String, String> languageValues = new HashMap<String, String>();
+
+    try {
+        String[] lines = Files.readAllLines(path);
+        for(String line : lines) {
+            String[] keyValuePair = line.split("=");
+            languageValues.put(keyValuePair[0], keyValuePair[1]);
+        }
+    } catch (IOException e) {
+
+    }
+
+    public static void main(String Args[]) {
+
+    }
+
 }
