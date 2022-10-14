3602dacb0de2365b200ddcf25c3c6db4d06877b9
sand4570 <sand4570@stud.kea.dk>
2022-10-14 11:25:10 +0200

    add danish langish

--- src/game/Languish.java
+++ src/game/Languish.java
@@ -5,24 +5,41 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.List;
 
 
 public class Languish {
 
-    Path path = Paths.get("english.txt");
-    HashMap<String, String> languageValues = new HashMap<String, String>();
+    private HashMap<String, String> languageValues;
 
-    try {
-        String[] lines = Files.readAllLines(path);
-        for(String line : lines) {
-            String[] keyValuePair = line.split("=");
-            languageValues.put(keyValuePair[0], keyValuePair[1]);
+    public Languish() {
+        Path path = Paths.get("/Users/sandie/Documents/DTU/CDIO2/src/game/english.txt");
+        languageValues = new HashMap<String, String>();
+
+        try {
+            List<String> lines = Files.readAllLines(path);
+            for(String line : lines) {
+                if (!line.trim().isEmpty()) {
+                    String[] keyValuePair = line.split("=");
+                    languageValues.put(keyValuePair[0], keyValuePair[1]);
+                }
+            }
+        } catch (IOException e) {
+
         }
-    } catch (IOException e) {
+    }
 
+    public String getLanguageValue(String key) {
+        return languageValues.get(key);
     }
 
     public static void main(String Args[]) {
+        System.out.println("Program starts here");
+
+        Languish currentLanguish = new Languish();
+        System.out.println(currentLanguish.getLanguageValue("field2"));
+
+
 
     }
 
