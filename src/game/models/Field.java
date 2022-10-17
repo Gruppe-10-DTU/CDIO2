0c5ca8ab3031a86ffc74fd385d09771ce4836850
A-bjoern <113096228+A-bjoern@users.noreply.github.com>
2022-10-17 10:49:04 +0200

    Changed field class so that it resembles what Nilas wrote.

--- src/game/models/Field.java
+++ src/game/models/Field.java
@@ -4,11 +4,19 @@ import game.models.*;
 
 public class Field {
     int[] effects = new int[]{250, -100, 100,-20,180,0,-70,60,-80,-50,650};
-    public Field(int value, String fieldNumber) {
-            int effect = effects[value];
+    int effect;
+    String fieldDesc;
+    public Field(int effect, String fieldDesc) {
+           this.effect = effect;
+           this.fieldDesc = fieldDesc;
 
     }
-
+    public int getEffect(){
+            return effect;
+}
+    public String getDesc(){
+            return fieldDesc;
+    }
 }
 
 
