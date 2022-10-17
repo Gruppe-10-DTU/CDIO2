1e1d178258293bd489e09d28c27e9fa137b5e232
NilasThoegersen <nilas96@gmail.com>
2022-10-17 12:21:33 +0200

    Removed unused import

--- src/game/buisness_logic/Start.java
+++ src/game/buisness_logic/Start.java
@@ -1,11 +1,8 @@
 package game.buisness_logic;
 
-import game.ui.Game;
-
 public class Start {
     public static void main(String[] args) {
         GameController miniMonopoly = new GameController("en");
-
         miniMonopoly.play();
     }
 }
