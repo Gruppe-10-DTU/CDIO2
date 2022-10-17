093c149bf3369658e04f2b50f38e3f840fa949eb
A-bjoern <113096228+A-bjoern@users.noreply.github.com>
2022-10-17 10:25:47 +0200

    Somehow forgot to commit and push field class.

---
+++ src/game/models/Field.java
@@ -0,0 +1,14 @@
+package game.models;
+import game.models.*;
+
+
+public class Field {
+    int[] effects = new int[]{250, -100, 100,-20,180,0,-70,60,-80,-50,650};
+    public Field(int value, String fieldNumber) {
+            int effect = effects[value];
+
+    }
+
+}
+
+
