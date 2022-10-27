f575bc246d462caf38fd2c6384d77185e09b038b
NilasThoegersen <nilas96@gmail.com>
2022-10-27 14:50:21 +0200

    Moved files around so we were able compile the project to a .jar file. Added manifest for the .jar compiling, moved files around and changed Language to use a inputstream so i could get an absolute path for the txt files.

--- src/game/models/Language.java
+++ src/game/models/Language.java
@@ -1,7 +1,11 @@
 package game.models;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
@@ -12,22 +16,10 @@ public class Language {
     private HashMap<String, String> languageValues;
 
     public Language() {
-        updateLanguage("src/game/english.txt");
+        updateLanguage("/game/src/game/english.txt");
     }
     public Language(String language){
-        String target;
-        switch (language){
-            case "English":
-                target = "src/game/english.txt";
-                break;
-            case "Danish":
-                target = "src/game/danish.txt";
-                break;
-            default:
-                target = "src/game/english.txt";
-                break;
-        }
-        updateLanguage(target);
+        updateLanguage(language);
     }
 
     //Returns the value to the requestes key, kan add extra string if the value includes {0}
@@ -45,20 +37,22 @@ public class Language {
         String target;
         switch (language){
             case "English":
-                target = "src/game/english.txt";
+                target = "english.txt";
                 break;
             case "Danish":
-                target = "src/game/danish.txt";
+                target = "danish.txt";
                 break;
             default:
-                target = "src/game/english.txt";
+                target = "english.txt";
                 break;
         }
-        Path path = Paths.get(target);
+        InputStream file = this.getClass().getResourceAsStream(target);
+
         languageValues = new HashMap<String, String>();
         try {
-            List<String> lines = Files.readAllLines(path);
-            for(String line : lines) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(file));
+            String line;
+            while((line = br.readLine()) != null) {
                 if (!line.trim().isEmpty()) {
                     String[] keyValuePair = line.split("=");
                     languageValues.put(keyValuePair[0], keyValuePair[1]);
