ad305be65405e9c87b4c5c62f0a9a17b3adb5729
NilasThoegersen <nilas96@gmail.com>
2022-10-28 10:32:38 +0200

    Removed the run command from the build.bat file.

--- src/build.bat
+++ src/build.bat
@@ -3,5 +3,4 @@ IF NOT EXIST "miniMatador.jar" (
 	javac game/*.java game/models/*.java game/controllers/*.java game/ui/*.java -cp game/matadorgui-3.1.6.jar
 	jar cmf game/META-INF/MANIFEST.MF miniMatador.jar game/*
 )
-java -jar miniMatador.jar
 exit /b
